19f6aec005c84fe2e2b17ac78d4912beb6bb23cd
declan <declan.wx.wong@gmail.com>
2026-09-05 12:03:06 +1000

    create draft c file

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define SERVO1_PIN = 18 // PWM pin for servo 1
+#define SERVO2_PIN = 19 // PWM pin for servo 2
+
+#define AIN1 = 
+
+void setup() {
+
+}
+
+void loop() {
+
+}
