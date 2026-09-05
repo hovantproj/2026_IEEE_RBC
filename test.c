7af338ac721ab53abe2ca91115e677aa3c729cb6
declan <declan.wx.wong@gmail.com>
2026-09-05 12:09:36 +1000

    add literals to test code

--- test.c
+++ test.c
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
-#define SERVO1_PIN = 18 // PWM pin for servo 1
-#define SERVO2_PIN = 19 // PWM pin for servo 2
+// define servo pins
+#define SERVO1 18 // PWM pin for servo 1
+#define SERVO2 19 // PWM pin for servo 2
 
-#define AIN1 = 
+// define motor pins
+#define AIN1 27
+#define AIN2 14
+#define APWM 25
+#define BIN1 13
+#define BIN2 12
+#define BPWM 26
+#define STBY 32
+
+// define IR pins
+#define IR1 34
+#define IR2 35
+
+// define US pins
+#define TRIGGER_PIN 23
+#define ECHO_PIN 26
+#define MAX_DISTANCE 50
 
 void setup() {
 
